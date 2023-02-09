c1652005360d5998cf16443f911cb73a1d8643d3
Rufaro Clarisa Gapare <rufaroclarisagapare@Rufaros-MacBook-Air.local>
2023-02-08 23:12:44 -0500

    cleanups

--- src/screens/HomeStack.tsx
+++ src/screens/HomeStack.tsx
@@ -1,4 +1,3 @@
-import { View, Text } from 'react-native';
 import React from 'react';
 import { HomeParamList } from '../navigation/types';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -11,10 +10,12 @@ const Stack = createNativeStackNavigator<HomeParamList>();
 
 export default function HomeStack() {
   return (
-    <Stack.Navigator
-      initialRouteName="Feed"
-    >
-      <Stack.Screen name="Feed" component={HomeScreen} options={{headerShown: false}}/>
+    <Stack.Navigator initialRouteName="Feed">
+      <Stack.Screen
+        name="Feed"
+        component={HomeScreen}
+        options={{ headerShown: false }}
+      />
       <Stack.Screen
         name="AddReceipt"
         component={AddReceipt}
